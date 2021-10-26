e4d21c6685dcc2c337ad51319ea1ecfe2b4d70c9
CodePingu <agrasrodrigo@gmail.com>
2021-10-25 20:35:20 -0400

    comment

---
+++ build/CMakeFiles/CFlexer.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/codepingu/Downloads/CFlexer/src/common/util.c" "/home/codepingu/Downloads/CFlexer/build/CMakeFiles/CFlexer.dir/src/common/util.c.o"
+  "/home/codepingu/Downloads/CFlexer/src/core/keyboard.c" "/home/codepingu/Downloads/CFlexer/build/CMakeFiles/CFlexer.dir/src/core/keyboard.c.o"
+  "/home/codepingu/Downloads/CFlexer/src/core/time.c" "/home/codepingu/Downloads/CFlexer/build/CMakeFiles/CFlexer.dir/src/core/time.c.o"
+  "/home/codepingu/Downloads/CFlexer/src/core/vec2.c" "/home/codepingu/Downloads/CFlexer/build/CMakeFiles/CFlexer.dir/src/core/vec2.c.o"
+  "/home/codepingu/Downloads/CFlexer/src/engine/game.c" "/home/codepingu/Downloads/CFlexer/build/CMakeFiles/CFlexer.dir/src/engine/game.c.o"
+  "/home/codepingu/Downloads/CFlexer/src/main.c" "/home/codepingu/Downloads/CFlexer/build/CMakeFiles/CFlexer.dir/src/main.c.o"
+  "/home/codepingu/Downloads/CFlexer/src/rendering/mesh.c" "/home/codepingu/Downloads/CFlexer/build/CMakeFiles/CFlexer.dir/src/rendering/mesh.c.o"
+  "/home/codepingu/Downloads/CFlexer/src/rendering/shader.c" "/home/codepingu/Downloads/CFlexer/build/CMakeFiles/CFlexer.dir/src/rendering/shader.c.o"
+  "/home/codepingu/Downloads/CFlexer/src/rendering/texture.c" "/home/codepingu/Downloads/CFlexer/build/CMakeFiles/CFlexer.dir/src/rendering/texture.c.o"
+  "/home/codepingu/Downloads/CFlexer/src/rendering/window.c" "/home/codepingu/Downloads/CFlexer/build/CMakeFiles/CFlexer.dir/src/rendering/window.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  "../glfw"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/CFlexer.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CFlexer"
+  "CFlexer.pdb"
+  "CMakeFiles/CFlexer.dir/src/common/util.c.o"
+  "CMakeFiles/CFlexer.dir/src/core/keyboard.c.o"
+  "CMakeFiles/CFlexer.dir/src/core/time.c.o"
+  "CMakeFiles/CFlexer.dir/src/core/vec2.c.o"
+  "CMakeFiles/CFlexer.dir/src/engine/game.c.o"
+  "CMakeFiles/CFlexer.dir/src/main.c.o"
+  "CMakeFiles/CFlexer.dir/src/rendering/mesh.c.o"
+  "CMakeFiles/CFlexer.dir/src/rendering/shader.c.o"
+  "CMakeFiles/CFlexer.dir/src/rendering/texture.c.o"
+  "CMakeFiles/CFlexer.dir/src/rendering/window.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CFlexer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
