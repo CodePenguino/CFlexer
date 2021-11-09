790ebfc99ad61f0245a07e0af8ad6c9ad26a7145
CodePingu <agrasrodrigo@gmail.com>
2021-11-08 20:34:08 -0500

    does this work?

---
+++ build/CMakeFiles/CFlexer.dir/src/core/m2.c.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/CFlexer.dir/src/core/m2.c.o: \
+ /home/codepingu/Programming/CFlexer/src/core/m2.c \
+ /usr/include/stdc-predef.h \
+ /home/codepingu/Programming/CFlexer/src/core/m2.h
---
+++ build/CMakeFiles/CFlexer.dir/src/core/transform.c.o.d
@@ -0,0 +1,67 @@
+CMakeFiles/CFlexer.dir/src/core/transform.c.o: \
+ /home/codepingu/Programming/CFlexer/src/core/transform.c \
+ /usr/include/stdc-predef.h \
+ /home/codepingu/Programming/CFlexer/src/core/transform.h \
+ /home/codepingu/Programming/CFlexer/src/core/math.h \
+ /home/codepingu/Programming/CFlexer/src/core/vec2.h \
+ /home/codepingu/Programming/CFlexer/src/core/vec3.h \
+ /home/codepingu/Programming/CFlexer/src/core/vec4.h \
+ /home/codepingu/Programming/CFlexer/src/core/m3.h \
+ /home/codepingu/Programming/CFlexer/src/core/m4.h \
+ /home/codepingu/Programming/CFlexer/src/core/../rendering/window.h \
+ /home/codepingu/Programming/CFlexer/include/GL/glew.h \
+ /usr/lib/gcc/x86_64-linux-gnu/9/include/stddef.h \
+ /usr/lib/gcc/x86_64-linux-gnu/9/include/stdint.h /usr/include/stdint.h \
+ /usr/include/x86_64-linux-gnu/bits/libc-header-start.h \
+ /usr/include/features.h /usr/include/x86_64-linux-gnu/sys/cdefs.h \
+ /usr/include/x86_64-linux-gnu/bits/wordsize.h \
+ /usr/include/x86_64-linux-gnu/bits/long-double.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs-64.h \
+ /usr/include/x86_64-linux-gnu/bits/types.h \
+ /usr/include/x86_64-linux-gnu/bits/timesize.h \
+ /usr/include/x86_64-linux-gnu/bits/typesizes.h \
+ /usr/include/x86_64-linux-gnu/bits/time64.h \
+ /usr/include/x86_64-linux-gnu/bits/wchar.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-intn.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-uintn.h /usr/include/GL/glu.h \
+ /usr/include/GL/gl.h \
+ /home/codepingu/Programming/CFlexer/include/GLFW/glfw3.h \
+ /home/codepingu/Programming/CFlexer/src/core/../rendering/../common/util.h \
+ /usr/include/math.h /usr/include/x86_64-linux-gnu/bits/math-vector.h \
+ /usr/include/x86_64-linux-gnu/bits/libm-simd-decl-stubs.h \
+ /usr/include/x86_64-linux-gnu/bits/floatn.h \
+ /usr/include/x86_64-linux-gnu/bits/floatn-common.h \
+ /usr/include/x86_64-linux-gnu/bits/flt-eval-method.h \
+ /usr/include/x86_64-linux-gnu/bits/fp-logb.h \
+ /usr/include/x86_64-linux-gnu/bits/fp-fast.h \
+ /usr/include/x86_64-linux-gnu/bits/mathcalls-helper-functions.h \
+ /usr/include/x86_64-linux-gnu/bits/mathcalls.h /usr/include/time.h \
+ /usr/include/x86_64-linux-gnu/bits/time.h \
+ /usr/include/x86_64-linux-gnu/bits/types/clock_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/time_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/struct_tm.h \
+ /usr/include/x86_64-linux-gnu/bits/types/struct_timespec.h \
+ /usr/include/x86_64-linux-gnu/bits/endian.h \
+ /usr/include/x86_64-linux-gnu/bits/endianness.h \
+ /usr/include/x86_64-linux-gnu/bits/types/clockid_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/timer_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/struct_itimerspec.h \
+ /usr/include/x86_64-linux-gnu/bits/types/locale_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__locale_t.h \
+ /home/codepingu/Programming/CFlexer/src/core/../rendering/../core/keyboard.h \
+ /usr/lib/gcc/x86_64-linux-gnu/9/include/stdbool.h /usr/include/stdio.h \
+ /usr/lib/gcc/x86_64-linux-gnu/9/include/stdarg.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__fpos_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__mbstate_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__fpos64_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__FILE.h \
+ /usr/include/x86_64-linux-gnu/bits/types/FILE.h \
+ /usr/include/x86_64-linux-gnu/bits/types/struct_FILE.h \
+ /usr/include/x86_64-linux-gnu/bits/stdio_lim.h \
+ /usr/include/x86_64-linux-gnu/bits/sys_errlist.h \
+ /home/codepingu/Programming/CFlexer/src/core/../rendering/mesh.h \
+ /home/codepingu/Programming/CFlexer/src/core/../rendering/texture.h \
+ /home/codepingu/Programming/CFlexer/src/core/../rendering/../core/transform.h \
+ /home/codepingu/Programming/CFlexer/src/core/../rendering/shader.h \
+ /home/codepingu/Programming/CFlexer/src/core/../rendering/../core/math.h
